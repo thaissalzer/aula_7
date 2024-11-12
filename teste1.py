193ef64b1a101a14097255e08f16983fce00b35e
Thais Salzer Procopio <106846359+thaissalzer@users.noreply.github.com>
2024-11-12 19:44:10 -0300

    Update teste1.py

--- teste1.py
+++ teste1.py
@@ -3,3 +3,11 @@ st.write("Sou servidor público!")
 
 
 st.write("Ola,mundo!")
+
+st.title("Este é o título do app")
+st.header("Este é o subtítulo")
+st.subheader("Este é o terceiro subtítulo")
+st.markdown("Este é texto")
+st.caption("Esta é a a legenda")
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
