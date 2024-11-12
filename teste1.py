96d484e460a135edbfef97709ca080577f40e91e
Thais Salzer Procopio <106846359+thaissalzer@users.noreply.github.com>
2024-11-12 19:45:39 -0300

    Update teste1.py

--- teste1.py
+++ teste1.py
@@ -11,3 +11,7 @@ st.markdown("Este é texto")
 st.caption("Esta é a a legenda")
 st.code("x=2021")
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+
+
+numero = st.slider('Selecione um número', min_value = 0, max_value = 100)
+st.text("Seu número é " + str(numero))
